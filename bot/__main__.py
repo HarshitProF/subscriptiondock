3a3bf60ea9cc7cc035dcb0122878b246f473c040
Harshit Pathak <pathakharshit64@gmail.com>
2023-03-30 06:18:11 +0100

    lkj

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,14 +1,16 @@
 from . import bot
 import threading
-from telebot import util
-from telebot.util.update_types import message,callback_query,my_chat_member,chat_member,chat_join_request
 import apscheduler
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
+from hanlder import tesks
+
 admin='741728025'
-from hanlder import welcome_handler,plan_handler,buy_handler,joined_handler,back,conntact,approve_or_reject,admin_handler,tesks,status,chat_member
 
 if __name__=="__main__":
     from register_handlers import register
-    register.register(bot)
+    register.register().add(bot)
+    job_stores={'defualt':SQLAlchemyJobStore(url="mysql://root:H@r$hit1@localhost:3306/subscription", tablename="jobs")}
     schedule=BackgroundScheduler(job_stores=job_stores,demon=True,timezone="Asia/Kolkata")
     schedule.add_job(func=tesks.reminder ,kwargs={'bot':bot},trigger='cron',hour='21',minute='36')
     schedule.start()
--- clock.py
+++
@@ -1,8 +0,0 @@
-from bot import bot
-from hanlder import tesks
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-job_stores={'defualt':SQLAlchemyJobStore(url="mysql://root:H@r$hit1@localhost:3306/subscription", tablename="jobs")}
-schedule=BackgroundScheduler(job_stores=job_stores,demon=True,timezone="Asia/Kolkata")
-schedule.add_job(func=tesks.reminder ,kwargs={'bot':bot},trigger='cron',hour='21',minute='36')
-print(schedule.get_jobs())
