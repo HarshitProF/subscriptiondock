3f0970ea443f6ab22424a597e639c9575537aac3
Harshit Pathak <pathakharshit64@gmail.com>
2023-03-31 08:03:40 +0100

    kdkf

--- bot/__main__.py
+++ bot/__main__.py
@@ -13,7 +13,7 @@ if __name__=="__main__":
     register.register().add(bot)
     job_stores={'defualt':SQLAlchemyJobStore(url="mysql://root:H@r$hit1@localhost:3306/subscription", tablename="jobs")}
     schedule=BackgroundScheduler(job_stores=job_stores,demon=True,timezone="Asia/Kolkata")
-    schedule.add_job(func=tesks.reminder ,kwargs={'bot':bot},trigger='cron',hour='23',minute='30')
+    schedule.add_job(func=tesks.reminder ,kwargs={'bot':bot},trigger='cron',hour='00',minute='30')
     schedule.start()
     print(schedule.get_jobs())
     bot.infinity_polling(allowed_updates=['message','callback_query','my_chat_member','chat_member'])
