b8eade8f31ec5fbe40dba6772d077f35fb40efac
Harshit Pathak <pathakharshit64@gmail.com>
2023-03-31 21:28:58 +0100

    yrdbvc

--- hanlder/tesks.py
+++ hanlder/tesks.py
@@ -16,7 +16,13 @@ def reminder(bot):
                 if k['end_date']==curdate :
                     bot.ban_chat_member(chat_id=chat_id,user_id=k['telegram_id'])
                     bot.send_message(k['telegram_id'],text="Your plan has expired")
-                elif (difference.days <= 4) and (difference.days>0) :
+                    try:
+                        user.user().set_status(telegram_id=k['telegram_id'],user_status="Banned")
+                    except Exception as e:
+                        print(e)
+                    text=f"Name :- {k['fname']} {k['lname']} \n\n Username :-@{k['username']} \n\n His plan has expired"
+                    bot.send_message(int(admin),text=text)
+                elif (difference.days <= 4) and (difference.days>=1) :
                     bot.send_message(k['telegram_id'],text="your subscription is expiring soon")
             else :
                 pass
