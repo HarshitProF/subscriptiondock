0b4b7d1eb82b8d140abd23927d04e2affd26aa75
Harshit Pathak <pathakharshit64@gmail.com>
2023-04-04 11:56:37 +0100

    hjgfg

--- bot/__main__.py
+++ bot/__main__.py
@@ -9,12 +9,14 @@ from hanlder import tesks
 #admin='741728025'
 chat_id=os.getenv('CHAT_ID',default=None)
 admin=os.getenv('ADMIN_ID',default=None)
+import datetime
 
 if __name__=="__main__":
+    print(datetime.datetime.now())
     from register_handlers import register
     register.register().add(bot)
     job_stores={'defualt':SQLAlchemyJobStore(url="mysql://root:H@r$hit1@localhost:3306/subscription", tablename="jobs")}
-    schedule=BackgroundScheduler(job_stores=job_stores,demon=True,timezone="Asia/Kolkata")
+    schedule=BackgroundScheduler(job_stores=job_stores,demon=True,timezone="Eastern time")
     schedule.add_job(func=tesks.reminder ,kwargs={'bot':bot},trigger='cron',hour='12',minute='00')
     schedule.start()
     print(schedule.get_jobs())
