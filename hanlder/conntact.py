05d4dc97a525b75a88f90bcb7a26184064fb9a78
Harshit Pathak <pathakharshit64@gmail.com>
2023-04-08 20:46:00 +0100

    fs

--- hanlder/conntact.py
+++ hanlder/conntact.py
@@ -5,7 +5,6 @@ admins=["@jhonnyCrypto","@LagannCrypto","@ZachCryptoAD","AlexRoseAdmin","@CHIEF0
 chat_id=os.getenv('CHAT_ID',default=None)
 admin=os.getenv('ADMIN_ID',default=None)
 def send_contact(message:Message,bot:TeleBot):
-    bot.send_message(message.from_user.id,text=f"Please contact to admin @jhonnyCrypto")
     if chat_id=="-1001691747162":
         bot.send_message(message.from_user.id,text=f"Please contact to admin {admins[2]}")
     elif chat_id=="-1001696317955":
