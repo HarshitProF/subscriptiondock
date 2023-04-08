c8d07e902ef4c4af2ebda28bd87897abeb822354
Harshit Pathak <pathakharshit64@gmail.com>
2023-04-08 20:42:24 +0100

    fhjs

--- hanlder/conntact.py
+++ hanlder/conntact.py
@@ -1,7 +1,18 @@
 from telebot import TeleBot
 from telebot.types import Message
 import os
+admins=["@jhonnyCrypto","@LagannCrypto","@ZachCryptoAD","AlexRoseAdmin","@CHIEF099"]
 chat_id=os.getenv('CHAT_ID',default=None)
 admin=os.getenv('ADMIN_ID',default=None)
 def send_contact(message:Message,bot:TeleBot):
     bot.send_message(message.from_user.id,text=f"Please contact to admin @jhonnyCrypto")
+    if chat_id=="-1001691747162":
+        bot.send_message(message.from_user.id,text=f"Please contact to admin {admins[2]}")
+    elif chat_id=="-1001696317955":
+        bot.send_message(message.from_user.id,text=f"Please contact to admin {admins[3]}")
+    elif chat_id=="-1001633570949":
+        bot.send_message(message.from_user.id,text=f"Please contact to admin {admins[4]}")
+    elif chat_id=="-1001566254717":
+        bot.send_message(message.from_user.id,text=f"Please contact to admin {admins[1]}")
+    else:
+        bot.send_message(message.from_user.id,text=f"Please contact to admin {admins[0]}")
