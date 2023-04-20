e2bd020e1de5944ee9df8ab7d184d6916844905c
Harshit Pathak <pathakharshit64@gmail.com>
2023-04-20 10:38:05 +0100

    sds

--- hanlder/chat_member.py
+++ hanlder/chat_member.py
@@ -18,8 +18,8 @@ def chat_member(message:ChatMemberUpdated,bot:TeleBot):
             bot.ban_chat_member(chat_id=chat_id,user_id=message.new_chat_member.id)
         except Exception as e:
             print(e)
-    curdate=date.today()
     else:
+        curdate=date.today()
         if  (not required_user['end_date']) or (required_user['end_date']<curdate) :
             try:
                 bot.ban_chat_member(chat_id=chat_id,user_id=message.new_chat_member.id)
