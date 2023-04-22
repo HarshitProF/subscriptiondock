1ee6fc0f7a96b9434ba025ef41859d1aac4df7a9
Harshit Pathak <pathakharshit64@gmail.com>
2023-04-22 06:53:19 +0100

    ds

--- hanlder/chat_member.py
+++ hanlder/chat_member.py
@@ -13,7 +13,7 @@ def chat_member(message:ChatMemberUpdated,bot:TeleBot):
     try:
         required_user=user.user().get_user_by_telegram_id(message.new_chat_member.id)
     except Exception as e:
-        dat=str(e).split("")
+        dat=str(e).split(" ")
         print(dat)
         if dat[0]=="user":
             try:
